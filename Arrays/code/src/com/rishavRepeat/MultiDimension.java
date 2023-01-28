7b8b25b75a7dee6cba6a3cc6434f1ea15f79e08f
rishavmehra <rishavmehra61@gmail.com>
2023-01-29 02:22:24 +0530

    Linear Search

--- Arrays/code/src/com/rishavRepeat/MultiDimension.java
+++ Arrays/code/src/com/rishavRepeat/MultiDimension.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class MultiDimension {
 
     public static void main(String[] args) {
-
-
         Scanner in = new Scanner(System.in);
 //        int[][] arr = {
 //                {1,2,3},
