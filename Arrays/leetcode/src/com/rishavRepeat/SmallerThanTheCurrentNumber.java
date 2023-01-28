8e9bfec8996d35e2c7afa29c5cb707e50c7ae673
rishavmehra <rishavmehra61@gmail.com>
2023-01-28 22:24:44 +0530

    SmallerThanTheCurrentNumber

---
+++ Arrays/leetcode/src/com/rishavRepeat/SmallerThanTheCurrentNumber.java
@@ -0,0 +1,17 @@
+package com.rishavRepeat;
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+// 1365. How Many Numbers Are Smaller Than the Current Number
+import java.util.Arrays;
+
+public class SmallerThanTheCurrentNumber {
+    public static void main(String[] args) {
+        int[] nums = {8,1,2,2,3};
+        System.out.println((Arrays.toString(smallerNumbersThanCurrent(nums))));
+    }
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+
+    }
+}
