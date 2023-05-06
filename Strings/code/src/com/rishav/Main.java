a1bf178dcac7af23b65a6538df196d89c09c3740
rishavmehra <rishavmehra61@gmail.com>
2023-05-06 15:25:52 +0530

    String and StringBuilder

---
+++ Strings/code/src/com/rishav/Main.java
@@ -0,0 +1,16 @@
+package com.rishav;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Rishav Mehra";
+//        System.out.println(name);
+
+        String a = "Rishav";
+        System.out.println(a);
+        a = "Mehra";
+        System.out.println(a);
+    }
+}
